c4d019bdf165ae4c25dac9949fae1113cdeae325
compfixengineer <compfixengineer@gmail.com>
2024-11-19 23:43:56 +0400

    add: menu and modal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import timer from  './modules/timer';
+import menu from  './modules/menu';
+import modal from  './modules/modal';
 
 
 timer('25 november 2024');
+menu();
+modal();
--- src/modules/two.js
+++
@@ -1,6 +0,0 @@
-const twoModule = () => {
-    const twoScriptMessage = 'two.js';
-    console.log(twoScriptMessage);
-}
-
-export default twoModule;
